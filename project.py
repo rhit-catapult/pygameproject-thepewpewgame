f8428e56f44b3a1f4c43c8bde6dbf93885e8286e
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2024-07-12 15:30:51 -0400

    Initial commit

---
+++ project.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+import random
+import time
+
+
+def main():
+    # turn on pygame
+    pygame.init()
+
+    # create a screen
+    pygame.display.set_caption("Cool Project")
+    # TODO: Change the size of the screen as you see fit!
+    screen = pygame.display.set_mode((640, 480))
+
+    # let's set the framerate
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            # TODO: Add you events code
+
+        # TODO: Fill the screen with whatever background color you like!
+        screen.fill((255, 255, 255))
+
+        # TODO: Add your project code
+
+        # don't forget the update, otherwise nothing will show up!
+        pygame.display.update()
+
+main()
