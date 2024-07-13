b4e630111c8b9fd853c544b406fa20027fe192f1
Poisonscythe <BeloreAV@rose-hulman@edu>
2024-07-13 09:48:51 -0400

    #commit

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import time
 def main():
     # turn on pygame
     pygame.init()
-#this is a comment
+
     # create a screen
     pygame.display.set_caption("Cool Project")
     # TODO: Change the size of the screen as you see fit!
