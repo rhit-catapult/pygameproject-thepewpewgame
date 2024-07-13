d7e885cae7bf74973223232c2a264f36b1f48418
Michael <michaelhenn104@gmail.com>
2024-07-13 09:46:45 -0400

    hey guys

--- project.py
+++ project.py
@@ -3,13 +3,13 @@ import sys
 import random
 import time
 
-
+# hey guys
 def main():
     # turn on pygame
     pygame.init()
 
     # create a screen
-    pygame.display.set_caption("Cool Project")
+    pygame.display.set_caption("pew|pew")
     # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((640, 480))
 
@@ -19,8 +19,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        clock.tick(60)
 
-            # TODO: Add you events code
+            # TODO: Add your event's code
 
         # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
